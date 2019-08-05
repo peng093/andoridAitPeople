3c140c7077df9076560ad0d477ffc9e8e694ad71
weiyoupeng <2394668226@qq.com>
2019-08-05 10:30:48 +0800

    优化显示更多

--- app/src/main/java/com/example/atpeople/myapplication/util/UtilMoreText.java
+++ app/src/main/java/com/example/atpeople/myapplication/util/UtilMoreText.java
@@ -29,11 +29,7 @@ public class UtilMoreText {
      * 文字颜色
      */
     private int mSpanTextColor;
-    /**
-     * 设置图片
-     */
-    private Drawable mDrawableOpen;
-    private Drawable mDrawableClose;
+
     /**
      * 文字
      */
@@ -58,23 +54,9 @@ public class UtilMoreText {
         mSpanTextColor=color;
         createString();
     }
-    /**
-     * @param textView      文本框
-     * @param oriMsg        原始文字
-     * @param drawableOpen  展开图标
-     * @param drawableColse 关闭图标
-     */
-    public UtilMoreText(final TextView textView, String oriMsg, Drawable drawableOpen, Drawable drawableColse,int lines) {
-        mTextView = textView;
-        mOriMsg = oriMsg + "XX";
-        mDrawableOpen = drawableOpen;
-        mDrawableClose = drawableColse;
-        mLines = lines;
-        createImg();
-    }
 
     /**
-     * 创建文本形式的结尾
+     * 计算文字行数，没有超出则返回
      */
     public void createString() {
         mTextView.setText(mOriMsg);
@@ -82,11 +64,12 @@ public class UtilMoreText {
             @Override
             public void run() {
                 int lines = mTextView.getLineCount();
-                Log.e(TAG, "显示行数: " + mTextView.getLineCount());
+                // Log.e(TAG, "显示行数: " + mTextView.getLineCount());
                 if (lines > mLines) {
                     //必须设置否则无效
                     mTextView.setMovementMethod(LinkMovementMethod.getInstance());
-                    mTextView.setText(compressedWithString());
+                    SpannableString content_more=compressedWithString();
+                    mTextView.setText(content_more);
                 } else {
                     mTextView.setText(mOriMsg);
                     return;
@@ -96,57 +79,13 @@ public class UtilMoreText {
         });
 
     }
-
     /**
-     * 创建图片形似的结尾
-     */
-    public void createImg() {
-        mTextView.setText(mOriMsg);
-        mTextView.post(new Runnable() {
-            @Override
-            public void run() {
-                int lines = mTextView.getLineCount();
-                Log.e(TAG, "显示行数: " + mTextView.getLineCount());
-                if (lines > mLines) {
-                    //必须设置否则无效
-                    mTextView.setMovementMethod(LinkMovementMethod.getInstance());
-                    mTextView.setText(compressedWithImg());
-                } else {
-                    mTextView.setText(mOriMsg);
-                    return;
-                }
-
-            }
-        });
-
-
-    }
-
-    class spanImgClickable extends ClickableSpan {
-        @Override
-        public void onClick(View widget) {
-            Log.i("colick", "onClick: ");
-            TextView textView = (TextView) widget;
-            if (spread) {//调用展开的方法
-                spread = false;
-                textView.setText(getSpannableImg(mOriMsg, mDrawableClose));
-
-            } else {
-                spread = true;
-                textView.setText(getSpannableImg(textView.getText().subSequence(0, mOriMsg.length() >> 1), mDrawableOpen));
-            }
-        }
-
-        @Override
-        public void updateDrawState(TextPaint ds) {
-            super.updateDrawState(ds);
-        }
-    }
-
+     * 重写点击事件
+     * */
     class SpanTextClickable extends ClickableSpan {
         @Override
         public void onClick(View widget) {
-            if (spread) {//调用展开的方法
+            if (spread) {
                 spread = false;
                 mTextView.setText(getSpannableString(mOriMsg + mTextClose, mTextClose));
             } else {
@@ -158,65 +97,41 @@ public class UtilMoreText {
         @Override
         public void updateDrawState(TextPaint ds) {
             ds.setColor(mSpanTextColor);
+            // 斜体
             ds.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC));
-            ds.setUnderlineText(false);    //去除超链接的下划线
+            // 去除超链接的下划线
+            ds.setUnderlineText(false);
         }
     }
 
-    private SpannableString compressedWithImg() {
-        mTextView.setLines(mLines);
-        // 取第三行的最后一个字符串的index，0是第一行
-        int end = mTextView.getLayout().getLineEnd(mLines-1);
-        mTextView.setLines(Integer.MAX_VALUE);
-        // 利用index，截取至第mLines行，然后留出开关图片，
-        return getSpannableImg(mOriMsg.substring(0, end), mDrawableOpen);
-    }
-
     /**
-     * 压缩字符串
-     *
-     * @return 富文本字符串
+     * 给textview设置行数
+     * 并获取限制行数最后一个索引位置
+     * 截取五个字符串，在末尾拼接+"更多"
+     * 然后马上去掉行数限制
+     * @return SpannableString
      */
     private SpannableString compressedWithString() {
         mTextView.setLines(mLines);
         // 取第三行的最后一个字符串的index，0是第一行
         int end = mTextView.getLayout().getLineEnd(mLines-1);
-        // 利用index，截取至第mLines行，然后留出开关文字
-        String resultText = mOriMsg.substring(0, end - 5) + mTextOpen;
+        // 截取需要拼接的文字长度+拼接的文字
+        String resultText = mOriMsg.substring(0, end - mTextOpen.length()) + mTextOpen;
         mTextView.setLines(Integer.MAX_VALUE);
-        // 这里应该跟行数关联起来
+        // 给拼接的文字加入点击事件
         return getSpannableString(resultText, mTextOpen);
     }
 
     /**
      * 文字
-     *
-     * @param text 文字
+     * @param text 所有文字
+     * @param "更多" 拼接的文字
      * @return SpannableString
+     * 给后面拼接的文字加入点击事件
      */
     private SpannableString getSpannableString(CharSequence text, String switchStr) {
         SpannableString spanableInfo = new SpannableString(text);
         spanableInfo.setSpan(new UtilMoreText.SpanTextClickable(), text.length() - switchStr.length(), text.length(),
-                Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-        return spanableInfo;
-    }
-
-    /**
-     * 设置富文本 图片
-     *
-     * @param text     文字
-     * @param drawable 图片
-     * @return SpannableString
-     */
-    private SpannableString getSpannableImg(CharSequence text, Drawable drawable) {
-        int len = text.length();
-
-        SpannableString spanableInfo = new SpannableString(text);
-        int drawHeight = drawable.getMinimumHeight();
-        drawable.setBounds(0, 0, drawHeight, drawHeight);
-        CenterAlignImageSpan imageSpan = new CenterAlignImageSpan(drawable);
-        spanableInfo.setSpan(imageSpan, len - 5, len, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spanableInfo.setSpan(new UtilMoreText.spanImgClickable(), text.length() - 5, text.length(),
                 Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         return spanableInfo;
     }
