ec62af3d00dba307a1179b18e0dbc43100ddcb77
weiyoupeng <2394668226@qq.com>
2019-08-08 09:02:11 +0800

    1.优化启动页功能

--- app/src/main/java/com/example/atpeople/myapplication/LaunchActivity.java
+++ app/src/main/java/com/example/atpeople/myapplication/LaunchActivity.java
@@ -3,19 +3,17 @@ package com.example.atpeople.myapplication;
 import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Typeface;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
-import com.codemybrainsout.onboarder.AhoyOnboarderCard;
 import com.example.atpeople.myapplication.main.MainActivity;
+import com.example.atpeople.myapplication.util.particleview.ParticleView;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
@@ -23,56 +21,48 @@ import io.reactivex.disposables.Disposable;
 /**
  * Create by peng on 2019/7/16
  */
-public class LaunchActivity extends AhoyOnboarderActivity {
-    private SharedPreferences mPreferences;
+public class LaunchActivity extends AppCompatActivity {
+    private ParticleView mPv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        mPv1 = (ParticleView) findViewById(R.id.pv_1);
+        // 前部分主文字
+        mPv1.mHostText="有朋自";
+        mPv1.mParticleText="远方来";
+        mPv1.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                goToIndex();
+            }
+        });
+
+        mPv1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPv1.startAnim();
+            }
+        }, 200);
         requestReadAndWritePermission();
-        AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("Work Assistant", "Helps you find task targets quickly.", R.drawable.backpack);
-        AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Work Report", "Dynamic information editing, and complete information reporting.", R.drawable.chalk);
-        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("Share", "Share your work with the team.", R.drawable.chat);
+    }
 
-        ahoyOnboarderCard1.setBackgroundColor(R.color.black_transparent);
-        ahoyOnboarderCard2.setBackgroundColor(R.color.black_transparent);
-        ahoyOnboarderCard3.setBackgroundColor(R.color.black_transparent);
 
-        List<AhoyOnboarderCard> pages = new ArrayList<>();
-
-        pages.add(ahoyOnboarderCard1);
-        pages.add(ahoyOnboarderCard2);
-        pages.add(ahoyOnboarderCard3);
-
-        for (AhoyOnboarderCard page : pages) {
-            page.setTitleColor(R.color.white);
-            page.setDescriptionColor(R.color.grey_200);
-            //page.setTitleTextSize(dpToPixels(12, this));
-            //page.setDescriptionTextSize(dpToPixels(8, this));
-            //page.setIconLayoutParams(width, height, marginTop, marginLeft, marginRight, marginBottom);
-        }
-
-        setFinishButtonTitle("Finish");
-        showNavigationControls(true);
-        setGradientBackground();
-
-        //set the button style you created
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            setFinishButtonDrawableStyle(ContextCompat.getDrawable(this, R.drawable.rounded_button));
-        }
-
-        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
-        setFont(face);
+    private void goToIndex(){
+        Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void  isFirst(){
         SharedPreferences jame = getSharedPreferences("jame", 0);
         boolean isFirst = jame.getBoolean("isFirst", true);
         if(isFirst){
-            setOnboardPages(pages);
             SharedPreferences.Editor edit = jame.edit();
             edit.putBoolean("isFirst", false);
             edit.commit();
         }else {
-            Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            goToIndex();
         }
     }
     private void requestReadAndWritePermission() {
@@ -104,11 +94,5 @@ public class LaunchActivity extends AhoyOnboarderActivity {
 
                     }
                 });
-    }
-    @Override
-    public void onFinishButtonPressed() {
-        Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
