2896bb6a6e31674ec9da5f6d86aa510e33b5da43
peng <2394668226@qq.com>
2020-05-11 18:39:11 +0800

    1.新增webview

--- app/src/main/java/com/example/atpeople/myapplication/ui/webView/MyWebView.java
+++ app/src/main/java/com/example/atpeople/myapplication/ui/webView/MyWebView.java
@@ -1,7 +1,9 @@
 package com.example.atpeople.myapplication.ui.webView;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -12,11 +14,21 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.example.atpeople.myapplication.R;
+import com.example.atpeople.myapplication.util.GsonUtil;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.kongzue.baseokhttp.HttpRequest;
 import com.kongzue.baseokhttp.listener.JsonResponseListener;
+import com.kongzue.baseokhttp.listener.ResponseListener;
 import com.kongzue.baseokhttp.util.JsonMap;
 import com.kongzue.baseokhttp.util.Parameter;
 import com.orhanobut.logger.Logger;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Create by peng on 2019/8/2
@@ -41,12 +53,32 @@ public class MyWebView extends AppCompatActivity {
         settings.setJavaScriptEnabled(true);
         // 设置允许JS弹窗
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
-        // 跨域
+        // 本地文件跨域
         settings.setAllowFileAccessFromFileURLs(true);
         // 允许浏览器调试
         WebView.setWebContentsDebuggingEnabled(true);
+
+        // 图片跨域问题修复
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            settings.setAllowUniversalAccessFromFileURLs(true);
+        }else{
+            try {
+                Class<?> clazz = settings.getClass();
+                Method method = clazz.getMethod("setAllowUniversalAccessFromFileURLs", boolean.class);
+                if (method != null) {
+                    method.invoke(settings, true);
+                }
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
 //        mContentWv.loadUrl("https://www.baidu.com/");
-        mContentWv.loadUrl("file:///android_asset/ochart/index.html");
+        mContentWv.loadUrl("file:///android_asset/newOchart/index.html");
         mContentWv.addJavascriptInterface(new WebAppInterface(), "Android");
         mContentWv.setWebViewClient(new WebViewClient() {
             @Override
@@ -72,20 +104,23 @@ public class MyWebView extends AppCompatActivity {
     }
     private void sendPost(){
         // context 如果不是 Activity，本次请求在异步线程返回
-        HttpRequest.POST(this, "/company/getOchartList", new Parameter().add("id", "4200"), new JsonResponseListener() {
+        HttpRequest.POST(this, "/company/getOchartList", new Parameter().add("id", "4200"), new ResponseListener() {
             @Override
-            public void onResponse(JsonMap main, Exception error) {
-                if (error == null) {
-                    String data=main.getString("data");
-                    Log.e("kkk", data);
-                } else {
-                    Toast.makeText(getBaseContext(), "请求失败", Toast.LENGTH_SHORT).show();
+            public void onResponse(String response, Exception error) {
+                JsonObject jsonObject = (JsonObject) new JsonParser().parse(response).getAsJsonObject();
+                String json=jsonObject.get("data").toString();
+                String base64Sign = null;
+                try {
+                    base64Sign = Base64.encodeToString(json.getBytes("utf-8"),Base64.NO_WRAP);
+                    mContentWv.evaluateJavascript("showInfoFromJava('"+base64Sign+"')",null);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
             }
         });
-        String token="b9b7ca83c4bf652484715e61ac38bdff";
-        int id=4200;
-        mContentWv.loadUrl("javascript:showInfoFromJava('" + id + "','" + token + "')");
+//        String token="b9b7ca83c4bf652484715e61ac38bdff";
+//        int id=4200;
+//        mContentWv.loadUrl("javascript:showInfoFromJava('" + id + "','" + token + "')");
     }
 
     public class WebAppInterface{
@@ -95,7 +130,7 @@ public class MyWebView extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    sendPost();
+                     sendPost();
                 }
             });
 
