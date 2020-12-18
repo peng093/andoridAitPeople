b58f7fc0939e79ce36e8a114aec34d31d9b03b8e
weiyoupeng <2394668226@qq.com>
2020-12-18 16:20:00 +0800

    1.实现自定义注解

--- app/src/main/java/com/example/atpeople/myapplication/ui/annotation/AnnotationActivity.java
+++ app/src/main/java/com/example/atpeople/myapplication/ui/annotation/AnnotationActivity.java
@@ -29,9 +29,12 @@ public class AnnotationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // 这行代码什么意思呢？用过xUtils的人都知道，也需要有这么一行代码
         // 自定义注解实现的逻辑都在InjectAll实现了
+
         InjectUtils.InjectAll(this);
 
-        tv_main.setText("自定义注解绑定view666");
+        tv_main.setText("绑定view的注解逻辑: 获取当前类的所有成员变量，然后过滤得到InjectView注解的变量，" +
+                "拿到变量就能拿到他的注解及被注入的值，通过反射拿到当前类的findViewById方法，" +
+                "调用这个方法并传入注入的值 返回一个Object对象，再把这个对象赋值给变量就完成绑定。\n 设置注解的好处在于，给你设置标记(注解)，那么获取你就会很容易，能统一实现相同的逻辑");
         btn_login.setText("登录再登录");
         btn_logoff.setText("注销在注销");
     }
--- app/src/main/java/com/example/atpeople/myapplication/ui/annotation/IOnClick.java
+++ app/src/main/java/com/example/atpeople/myapplication/ui/annotation/IOnClick.java
@@ -7,12 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)   // 注解应用于其他注解上
+@Target(ElementType.METHOD)                         // 注解应用于其他注解上
 @Retention(RetentionPolicy.RUNTIME)
-// 这个注解需要注意,对应的参数在上面注解的 btn_login 点击事件里对应找即可
-@BaseEvent(setListener = "setOnClickListener",// setOnClickListener为View.setOnClickListener
-        listenerType = View.OnClickListener.class,// 监听的类型为点击事件
-        listenerCallback = "onClick")// 这个onClick回调，即为setOnClickListener后回调的onClick
+@BaseEvent(setListener = "setOnClickListener",      // setOnClickListener为View.setOnClickListener
+        listenerType = View.OnClickListener.class,  // 监听的类型为点击事件
+        listenerCallback = "onClick")               // 这个onClick回调，即为setOnClickListener后回调的onClick
 public @interface IOnClick {
     int[] value();// 因为一个方法可能与多个控件绑定
 }
